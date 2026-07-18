ccdd05367c5a2e46b579fbf00b7c0b6cea0054c3
Yomesh Nagar <yomeshnagar2006@gmail.com>
2026-07-18 21:17:49 +0530

    DevDocs

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express()
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://dev-docs-three.vercel.app"], 
     credentials: true,
   })
 );
@@ -30,7 +30,6 @@ app.use(express.urlencoded({
 }));
 
 app.use(cookieParser());
-app.use(cookieParser())
 
 
 // routes
